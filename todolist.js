1eb37f5a02a19adfeb220c1cef6bb8e9d370c3e7
shushogrig <44017123+shushogrig@users.noreply.github.com>
2019-03-02 11:31:45 +0400

    Create todolist.js

---
+++ todolist.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  let add = $('#add-group');
+  let name = $('.group-name');
+  let container = $('.group-column-header');
+  console.log('dddd')
+  let tasksList = $('.tasks')
+  let countTaskGroup = 0;
+  add.on('click', function () {
+    if(name.val()){
+      $(".group-column").append("<div class='tasks'><h1>"+name.val()+"</h1><input class='tasks-name'></input><button class='add-list'>ADD</input></div>");
+    } else{
+      alert('Please insert group name')
+    }
+  let tasks = $(".tasks")
+  let addList = $('.add-list');
+  let listName =$('.tasks-name');
+  console.log('tasks',tasks, tasks.length)
+  if (tasks.length>1){
+    let x,y, newx, newy,move
+    console.log('ddd',$(tasks))
+    $(tasks).on('mousedown',function(el){
+      if (el.target.className =='new-task'){
+        move=true;
+      }
+    })
+    $(tasks).on('onmouseup',function(el){
+      if (el.target.className =='new-task'){
+        move=false;
+      }
+    })
+    $(tasks).on('mousemove',function(el){
+      if(move){
+       if (el.target.className =='new-task') {
+          console.log('k',el.target)
+          x = el.offsetX;
+          y = el.offsetY;
+          console.log('x',x)
+          newx = el.pageX - x;
+          newy = el.pageY - y;
+          console.log('x',newx)
+          this.style.left = newx + 'px';
+          this.style.top = newy + 'px';
+        }
+      }
+     
+    });
+  }
+  addList.on('click', function () {
+    if(listName.val()){
+      let newTask = document.createElement("input");
+      newTask.className = "new-task";
+      newTask.setAttribute("data-id",tasks.length)
+        this.parentElement.append(newTask);
+      } else{
+        alert('Please insert task name')
+      }
+   });
+  });
+})
